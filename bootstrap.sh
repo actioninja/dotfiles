8c62e5106323afc6d2b5b1b1c91ff912bed021c6
actioninja <actioninja@criticalaction.net>
2025-06-22 19:44:56 -0700

    mise proper config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-mkdir -p ~/.local/bin
-cp ./.bootstrap/comtrya-x86linux-v0.9.2 ~/.local/bin/comtrya
-./.bootstrap/comtrya-x86linux-v0.9.2 -d ~/.dotfiles apply
+./shell/files/bin/comtrya -d ~/.dotfiles apply
