4d13482e76254ab2d615a8fb34bc66404778c19a
actioninja <actioninja@criticalaction.net>
2025-10-27 23:12:10 -0700

    various stuff

--- cli/files/fish/config.fish
+++ cli/files/fish/config.fish
@@ -12,6 +12,12 @@ set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
 
+if command -q nix-your-shell
+    nix-your-shell fish | source
+end
+
+mise activate fish | source
+
 set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense'
 carapace _carapace fish | source
 
@@ -19,7 +25,6 @@ set sponge_purge_only_on_exit true
 
 test -r $__fish_config_dir/dircolors; and eval (dircolors $__fish_config_dir/dircolors | sed 's/^LS_COLORS=/set -x LS_COLORS /; s/;$//')
 
-mise activate fish | source
 
 function __tide_mise_hack -v PWD
     _tide_remove_unusable_items
