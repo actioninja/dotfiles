ca12b660c6a9d157c6792c805616159752f2630a
Rob Bailey (actioninja) <actioninja@criticalaction.net>
2026-06-10 22:07:19 -0700

    Bunch more dotfiles tracked

--- cli/.config/fish/_dotfile_deploy_desktop.fish
+++ cli/.config/fish/_dotfile_deploy_desktop.fish
@@ -1,1 +1,6 @@
 mkdir -p ~/.config/sublime-text/Packages/
+dotfile_link desktop/.config/nheko ~/.config/nheko
+dotfile_link desktop/.config/sublime-text/Packages/User ~/.config/sublime-text/Packages/User
+dotfile_link desktop/.config/SeaDrive ~/.config/SeaDrive
+dotfile_link desktop/.config/krokiet ~/.config/krokiet
+dotfile_link desktop/.config/fontconfig ~/.config/fontconfig
