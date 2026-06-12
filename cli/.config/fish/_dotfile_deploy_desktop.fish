70eb0ee5bb4966c271e936d745eca32ebe408a28
Rob Bailey (actioninja) <actioninja@criticalaction.net>
2026-06-12 13:25:58 -0700

    Add fish function for adding new local binaries

--- cli/.config/fish/_dotfile_deploy_desktop.fish
+++ cli/.config/fish/_dotfile_deploy_desktop.fish
@@ -1,4 +1,5 @@
 mkdir -p ~/.config/sublime-text/Packages/
+dotfile_link desktop/.ideavimrc ~/.ideavimrc
 dotfile_link desktop/.config/nheko ~/.config/nheko
 dotfile_link desktop/.config/sublime-text/Packages/User ~/.config/sublime-text/Packages/User
 dotfile_link desktop/.config/SeaDrive ~/.config/SeaDrive
