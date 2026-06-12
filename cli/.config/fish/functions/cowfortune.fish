71624e0f724285936394933a1717ed76ff8e369b
Rob Bailey (actioninja) <actioninja@criticalaction.net>
2026-06-12 15:28:57 -0700

    More minor cleanup+features

--- cli/.config/fish/functions/cowfortune.fish
+++ cli/.config/fish/functions/cowfortune.fish
@@ -1,3 +1,7 @@
 function cowfortune --wraps='fortune -a | cowsay | lolcat -t'
-  fortune -a | cowsay | lolcat -t
+  set the_fortune "You're missing fortune, so instead I'm just here to look nice."
+  if command -q fortune
+    set the_fortune (fortune -a)
+  end
+  echo $the_fortune | cowsay | lolcat -t
 end
--- cli/.config/fish/functions/dotfile_addbin.fish
+++ cli/.config/fish/functions/dotfile_addbin.fish
@@ -1,5 +1,5 @@
 function dotfile_addbin
     set resolved (path resolve $argv[1])
-    7zz a $DOTFILE_HOME/cli/.local/bins.7z $resolved
+    7zz a -mx=9 m0=LZMA2 -mmt=on $DOTFILE_HOME/cli/.local/bins.7z $resolved
     mv $resolved $DOTFILE_HOME/cli/.local/bin
 end
