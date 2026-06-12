c4d66a3c6ce31f24432181e2ee37f0ba227d6372
actioninja <actioninja@criticalaction.net>
2026-06-12 01:54:47 -0700

    Compressed bins

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ scriptdir=$(cd "$(dirname -- "$0")" ; pwd -P)
 
 rm -rf ~/.config/fish
 ln -sfv "${scriptdir}/cli/.config/fish" ~/.config/fish
+$scriptdir/cli/.local/bin/7zz e -y -o$scriptdir/cli/.local/bin/ $scriptdir/cli/.local/bins.7z
 
-"${scriptdir}/cli/.local/bin/fish" "dotfile_deploy cli"
+$scriptdir/cli/.local/bin/fish -c "dotfile_deploy cli"
